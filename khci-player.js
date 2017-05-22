9aabe8db2fb17bb0d16ce12b9bc1741e30170338
hskimbusan <hskimbusan@kaist.ac.kr>
2017-05-22 13:10:16 +0900

    Add files via upload

--- khci-player.js
+++ khci-player.js
@@ -1,7 +1,3 @@
 $(document).ready(function(){
-	var c = document.getElementById("Record");
-	var ctx = c.getContext("2d");
-	ctx.beginPath();
-	ctx.arc(95, 50, 40, 0, 2*Math.PI);
-	ctx.stroke();
+
 });
