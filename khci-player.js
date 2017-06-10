370f5d573a4ecacffb628d90a9d1c23d937cd173
Kim Hyun Seong <hskimbusan@kaist.ac.kr>
2017-06-10 22:45:26 +0900

    playlist 수정

--- khci-player.js
+++ khci-player.js
@@ -1,28 +1,40 @@
 $(document).ready(function(){
 	var audio = new Audio();
 	
-	$("#b4").click(function(){
-		audio.pause();
-		audio = new Audio('./audio/b4.mp3');
-		audio.play();
-		var c = document.getElementById("Record");
-		var ctx = c.getContext("2d");
-
-		ctx.beginPath();
-		ctx.arc(250, 150, 100, 0, 2*Math.PI);
-		ctx.stroke();
-		ctx.fillStyle = "black";
-		ctx.fill();
-
-		var album = c.getContext("2d");
-		var img = document.getElementById("b4");
-		album.drawImage(img, 170, 70, 160, 160);
+	var title_list = [];
+	var play_list = [];
+	
+	for (i = 0; i < songs.length; i++){
+		title_list.push(songs[i]["title"]);
+	}
+	console.log(title_list);
+	
+	function clearPlaylist(){
+		title_list = [];
+		$("#playlist").html("");
+	}
+	
+	$("#search_input").autocomplete({
+		minLength: 2,
+		source: title_list,
+		select: function(event, ui){
+			for (i = 0; i < songs.length; i++){
+				if (ui.item.value == songs[i]["title"]){
+					document.getElementById("playlist").innerHTML +=
+						'<img id="song" data-selector='+play_list.length+' width="140" height="140" src='+songs[i]["image"] +' draggable="true">';
+					play_list.push(songs[i]);
+				}
+			}
+			return false;
+		}
 	});
 	
-	$("#bb").click(function(){
+    $(document).on("click", "#song", function() {
+        var i = $(this).data('selector');
 		audio.pause();
-		audio = new Audio('./audio/bb.mp3');
+		audio = new Audio(play_list[i].audio);
 		audio.play();
+		console.log(play_list[i]);
 		
 		var c = document.getElementById("Record");
 		var ctx = c.getContext("2d");
@@ -34,27 +46,10 @@ $(document).ready(function(){
 		ctx.fill();
 
 		var album = c.getContext("2d");
-		var img = document.getElementById("bb");
+		var img = new Image();
+		img.src = play_list[i].image;
 		album.drawImage(img, 170, 70, 160, 160);
-	});
-	
-	$("#akmu").click(function(){
-		audio.pause();
-		audio = new Audio('./audio/akmu.mp3');
-		audio.play();
-		var c = document.getElementById("Record");
-		var ctx = c.getContext("2d");
-
-		ctx.beginPath();
-		ctx.arc(250, 150, 100, 0, 2*Math.PI);
-		ctx.stroke();
-		ctx.fillStyle = "black";
-		ctx.fill();
-
-		var album = c.getContext("2d");
-		var img = document.getElementById("akmu");
-		album.drawImage(img, 170, 70, 160, 160);
-	});
+    });
 	
 	$("#play").click(function(){
 		audio.play();
@@ -66,4 +61,6 @@ $(document).ready(function(){
 		audio.pause();
 		audio.currentTime = 0;
 	});
+	
+	clearPlaylist();
 });
